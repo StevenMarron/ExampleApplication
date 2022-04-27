fc36a2ed9a8d451db3ae01d2785b41f7e5f384e7
StevenMarron <stevomarron@hotmail.com>
2022-04-27 19:53:15 +0100

    cleaned up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,15 +24,3 @@ docker cp ./privatekey.pem node_app:/privatekey.pem
 docker cp ./server.crt node_app:/server.crt 
 
 docker start node_app
-
-
-
-
-
-# sudo apt update && sudo apt install nodejs npm
-# sudo npm install -g pm2
-# pm2 stop example_app
-# cd ExampleApplication/
-# npm install
-
-# pm2 start ./bin/www --name example_app
